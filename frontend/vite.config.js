dcbe5149398d70180fef385106ebb8c7bf623faa
yuvaraj <yuvaraj.codes@gmail.com>
2026-06-19 17:10:24 +0530

    config for vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/outpass-portal/',
   server: {
     port: 5173,
     proxy: {
